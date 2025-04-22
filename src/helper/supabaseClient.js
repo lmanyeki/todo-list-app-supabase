7130de325964436ef4d0989119157b0f311797ad
lmanyeki-debug <lydiahmanyeki@gmail.com>
2025-04-22 09:52:01 +0300

    Corrects variables

--- src/helper/supabaseClient.js
+++ src/helper/supabaseClient.js
@@ -1,7 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://ttpjcltiieudcmevgkgj.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0cGpjbHRpaWV1ZGNtZXZna2dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUxNDI4NzcsImV4cCI6MjA2MDcxODg3N30.AZO4t2HSxxRqHt8fgezJHgENAnXrP3f7tkK8cBPo2zo";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
